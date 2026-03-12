6746e59bdce1705497edb7d96f76829986bc764d
Mamatkarimov ibrohim <ibrohim09.ny@gamil.com>
2026-03-12 09:02:16 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.css"
 const App = () => {
+  let [cnt, setSnt] = useState(0)
+  let [value, setValue] = useState()
+  let [data, setData] = useState([cnt])
+
+  function hendelPlus() {
+    setSnt(cnt + 1)
+
+    setData([...data, cnt + 1])
+  }
+
+  function hendelMinus() {
+    setSnt(cnt - 1)
+
+    setData([...data, cnt - 1])
+  }
+
   return (
     <div>
-      
+      <h1 className=''>{cnt}</h1>
+      <button className='w-10 h-10 bg-gray-50' onClick={hendelPlus}>+</button>
+      <button className='w-10 h-10 bg-gray-50' onClick={hendelMinus}>-</button>
+
+      <input className='border-1' type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+      <h1 className='text-center'>{value}</h1>
+      <div className='flex justify-center gap-[30px] flex-wrap pl-10 pr-10'>
+        {data.map((el) => {
+          return <h1 className='text-[34px]'>{el}</h1>
+        })}
+      </div>
     </div>
   )
 }
